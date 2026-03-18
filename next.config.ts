59f5ec442d5d1907aede42ac7becaa784f291787
binh.buidang <dangbinh4869@gmail.com>
2026-03-18 17:25:14 +0700

    fix kuromori

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
+  outputFileTracingIncludes: {
+    "/**": ["./node_modules/kuromoji/dict/**"],
+  },
 };
 
 export default nextConfig;
--- src/api/index.ts
+++ src/api/index.ts
@@ -1,6 +1,7 @@
 import { zValidator } from "@hono/zod-validator";
 import { desc, eq, ilike, isNotNull, or } from "drizzle-orm";
 import { Hono } from "hono";
+import { logger } from "hono/logger";
 import { z } from "zod";
 import { db } from "@/db";
 import { songs } from "@/db/schema";
@@ -177,7 +178,8 @@ const app = new Hono()
     try {
       const song = await processAndSave(c.req.valid("json"));
       return c.json(songResponse(song));
-    } catch {
+    } catch (e) {
+      console.log({ e });
       return c.json({ error: "Lyrics not found" }, 404);
     }
   })
@@ -220,5 +222,6 @@ const app = new Hono()
     },
   );
 
+app.use(logger());
 export type AppType = typeof app;
 export default app;
